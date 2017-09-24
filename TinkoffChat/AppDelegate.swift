65c895a569ee927aa3073844c8deaec53d42e89d
Furycateur <furycateur@furycateurs-macbook-pro.local>
2017-09-24 22:56:57 +0300

    redesigned

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -8,119 +8,70 @@
 
 import UIKit
 
+enum ApplicationStateInfo: String {
+    case active = "Active"
+    case inactive = "Inactive"
+    case background = "Background"
+    
+    init(state: UIApplicationState) {
+        switch state {
+        case .active:
+            self = .active
+        case .inactive:
+            self = .inactive
+        case .background:
+            self = .background
+        }
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var fromState: String = "Inactive"
     
-    
-    
-    
-    
-    var state = UIApplication.shared.applicationState
+    private var applicationStateInfo: ApplicationStateInfo = .inactive {
+        didSet {
+            print("Application moved from \(oldValue) to \(applicationStateInfo) state")
+        }
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
-        
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
+
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
- 
-        
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         let state = UIApplication.shared.applicationState
-        switch state {
-        case .active:
-            print("Application moved from \(fromState) to Active state: \(#function)")
-            fromState = "Active"
-        case .inactive:
-            print("Application moved from \(fromState) to Inactive state: \(#function)")
-            fromState = "Inactive"
-        case .background:
-            print("Application moved from \(fromState) to Background state: \(#function)")
-            fromState = "Background"
-        }
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        self.applicationStateInfo = ApplicationStateInfo(state: state)
+        print( "State was changed in: \(#function)")
     }
-
-
 }
 
--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -8,47 +8,47 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
+final class ProfileViewController: UIViewController {
+    
+    // MARK : Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-          print(#function)
-        // Do any additional setup after loading the view, typically from a nib.
+        print(#function)
     }
     
     override func viewWillAppear(_ animated: Bool) {
-         print(#function)
+        super.viewWillAppear(animated)
+        print(#function)
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-         print(#function)
-        
+        super.viewDidAppear(animated)
+        print(#function)
     }
     
     override func viewWillLayoutSubviews() {
-         print(#function)
+        super.viewWillLayoutSubviews()
+        print(#function)
     }
     
     override func viewDidLayoutSubviews() {
-         print(#function)
+        super.viewDidLayoutSubviews()
+        print(#function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-         print(#function)
+        super.viewWillDisappear(animated)
+        print(#function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-         print(#function)
+        super.viewDidDisappear(animated)
+        print(#function)
     }
-    
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print(#function)
     }
-
-
 }
 
