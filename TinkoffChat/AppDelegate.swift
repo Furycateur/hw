a9bc2ab0c51e0d7238727ce2115c1ec54068c471
Vlad Komissarov <drakula224@mail.ru>
2017-10-02 07:24:03 +0300

    2-nd homework

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -8,70 +8,41 @@
 
 import UIKit
 
-enum ApplicationStateInfo: String {
-    case active = "Active"
-    case inactive = "Inactive"
-    case background = "Background"
-    
-    init(state: UIApplicationState) {
-        switch state {
-        case .active:
-            self = .active
-        case .inactive:
-            self = .inactive
-        case .background:
-            self = .background
-        }
-    }
-}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
-    private var applicationStateInfo: ApplicationStateInfo = .inactive {
-        didSet {
-            print("Application moved from \(oldValue) to \(applicationStateInfo) state")
-        }
-    }
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
-
+        // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
-        let state = UIApplication.shared.applicationState
-        self.applicationStateInfo = ApplicationStateInfo(state: state)
-        print( "State was changed in: \(#function)")
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
 }
+
 
--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -8,47 +8,160 @@
 
 import UIKit
 
-final class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
+    @IBOutlet weak var uploadPhotoIcon: UIButton!
+    @IBOutlet weak var photoPlace: UIImageView!
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    let photoPicker = UIImagePickerController()
+  
+    
+    @IBAction func tapEditPhotoIcon(sender: UIButton!){
+       print("Выбери изображение профиля")
+        let userPictureActionSheet = UIAlertController(title: "Choose ur best Photo", message: nil, preferredStyle: .actionSheet)
+        showListOfActions(userPictureActionSheet)
+        present(userPictureActionSheet, animated: true)
+    }
+    
+    
+    
+    
+    func showListOfActions(_ sheet: UIAlertController) {
+        sheet.addAction(UIAlertAction(title: "Take photo", style: .default) {
+            [unowned self] action in
+            
+            if !UIImagePickerController.isSourceTypeAvailable(.camera){
+                let alertController = UIAlertController.init(title: nil, message: "У симулятора камеры нет, лул 😭", preferredStyle: .alert)
+                let okAction = UIAlertAction.init(title: "Понятненько", style: .default, handler: {(alert: UIAlertAction!) in
+                })
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+                
+            else{
+            self.photoPicker.allowsEditing = false
+            self.photoPicker.sourceType = UIImagePickerControllerSourceType.camera
+            self.photoPicker.cameraCaptureMode = .photo
+            self.photoPicker.modalPresentationStyle = .fullScreen
+            self.present(self.photoPicker, animated: true)
+            }
+        })
+        
+        sheet.addAction(UIAlertAction(title: "Select from library", style: .default) {
+            [unowned self] action in
+            
+            self.photoPicker.allowsEditing = false
+            self.photoPicker.sourceType = .photoLibrary
+            self.photoPicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            self.present(self.photoPicker, animated: true)
+        })
+        
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel) { action in })
+    }
+    
+    
+
+    
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        var  chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoPlace.image = chosenImage
+        dismiss(animated:true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ photoPicker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
     // MARK : Lifecycle
     override func viewDidLoad() {
+        
+        let radiusOfIconCorners = uploadPhotoIcon.frame.size.width / 2
+        uploadPhotoIcon.layer.cornerRadius = radiusOfIconCorners
+        photoPlace.layer.cornerRadius = radiusOfIconCorners
+        uploadPhotoIcon.clipsToBounds = true
+        photoPlace.clipsToBounds = true
+
+        
+        editProfileButton.layer.borderWidth = 2
+        editProfileButton.layer.cornerRadius = 8
+        editProfileButton.layer.borderColor = UIColor.black.cgColor
+        
+        let frameForViewDidLoad = editProfileButton.frame
+        print("Edit button frame \(frameForViewDidLoad) from viewDidLoad")
+        
         super.viewDidLoad()
-        print(#function)
+        photoPicker.delegate = self
     }
+
+   
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print(#function)
+        
+        let frameForViewWillAppear = self.editProfileButton.frame
+        print("Edit button frame \(frameForViewWillAppear) viewWillAppear")
+        print("Не отличается 🦄")
+        //print(#function)  — 1st hw
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print(#function)
+        //print(#function)  — 1st hw
+        
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        print(#function)
+       // print(#function)  — 1st hw
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        print(#function)
+        //print(#function)  — 1st hw
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print(#function)
+        //print(#function)  — 1st hw
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print(#function)
+        //print(#function)  — 1st hw
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        print(#function)
+        //print(#function)  — 1st hw
     }
+    
+    
+
+    
+
+    
+ 
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
